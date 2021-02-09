005667a6fc6e51033f04d4f96e3f74b2f48df45b
Aurore <aurore.gaczol@gmail.com>
2021-02-09 14:35:21 +0100

    Main avec / au lieu de /home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/home", name="main_home")
+     * @Route("/", name="main_home")
+     * @param SortieRepository $sortieRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
-        return $this->render('main/home.html.twig');
+        $sorties = $sortieRepository->findAll();
+        return $this->render('main/home.html.twig', [
+            "sorties" => $sorties
+        ]);
     }
 }
