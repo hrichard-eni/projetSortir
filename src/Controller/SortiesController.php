98cfaa02db710f8fe5c0efa4851da8848951761b
alexandre.bouchet2020@campus-eni.fr <-AlexB2951->
2021-02-10 10:28:47 +0100

    Ajout du formulaire à /newSortie (non termine non testé)

---
+++ src/Controller/SortiesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\NewSortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortiesController extends AbstractController
+{
+    /**
+     * @Route("/newSortie", name="sorties_new")
+     */
+    public function newSortie(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $sortie = new Sortie();
+        $form = $this->createForm(NewSortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //TODO : Préremplir l'organisateur et l'état
+            $sortie->setOrganisateur($this->getUser()->getPseudo);
+
+            //Envoi à la BDD
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('sorties/newSortie.html.twig', [
+            'newSortie' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/NewSortieType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie'
+            ])
+            ->add('duree', TimeType::class, [
+                'label' => 'Durée'
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Date de fin d\'inscription'
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombre max de participants'
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description et infos complémentaires'
+            ])
+//            ->add('organisateur')
+//            ->add('participants')
+//            ->add('campusOrganisateur')
+            ->add('lieu', EntityType::class, [
+                'class' => 'App\Entity\Lieu',
+                'invalid_message' => 'Eh le pirate ! Pas touche à notre code !',
+                'choice_label' => 'nom',
+                'placeholder' => 'Choisir un lieu '
+            ])
+//            ->add('etat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
