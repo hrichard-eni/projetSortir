f09bb6cbbbe8105f359894f4b36500489cf967cf
Aurore <aurore.gaczol@gmail.com>
2021-02-12 15:10:23 +0100

    s'inscrire // premiere version (sans contrainte)

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Participant;
+use App\Entity\Sortie;
 use App\Form\ParticipantFormType;
 use App\Repository\ParticipantRepository;
 use claviska\SimpleImage;
@@ -15,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class ParticipantController extends AbstractController
 {
@@ -97,6 +99,7 @@ class ParticipantController extends AbstractController
             "participantSelect" => $participantSelect
         ]);
     }
+
 
 
 }
---
+++ src/Controller/ParticipantSortieController.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Client\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ParticipantSortieController extends AbstractController
+{
+    private $participants;
+
+    /**
+     * @Route("/inscriptionSortie/{id}", name="inscription_sortie",methods={"GET"}, requirements={"id": "\d+"})
+     * @return string
+     */
+    public function ajouterParticipant($id, EntityManagerInterface $entityManager): Response
+    {
+
+        //qui est le participant ?
+        $user = $this->getUser();
+
+        $participantsortie = $entityManager->getRepository(Participant::class)->find($user->getId());
+        $sortieSelectionnee = $entityManager->getRepository(Sortie::class)->find($id);
+        //j'appelle la methode prévue par l'entity Sortie
+             $sortieSelectionnee->addParticipant($participantsortie);
+             $entityManager->persist($sortieSelectionnee);
+             $entityManager->flush();
+            $this->addFlash('success', 'Inscription réussie');
+        return $this->redirectToRoute("sorties_detail", ['id'=> $id]);
+            }
+
+
+
+
+
+
+
+
+}
